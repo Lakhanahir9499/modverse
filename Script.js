8b2564b0a9fb86ae97649146967dacbff9414546
Lakhanahir9499 <lakhanlakhnotra@protonmail.com>
2025-04-29 22:05:01 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,6 +1,10 @@
 <script>
-    function toggleMenu() {
-        const menu = document.getElementById("sideMenu");
-        menu.classList.toggle("active");
+  function toggleMenu() {
+    const menu = document.getElementById("navMenu");
+    if (menu.style.left === "0px") {
+      menu.style.left = "-220px";
+    } else {
+      menu.style.left = "0px";
     }
+  }
 </script>
