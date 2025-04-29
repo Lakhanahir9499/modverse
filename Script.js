19677dc26572034ca13287b51e07a87c6c1838fb
Lakhanahir9499 <lakhanlakhnotra@protonmail.com>
2025-04-29 21:52:15 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,6 +1,6 @@
 <script>
-function toggleMenu() {
-    const menu = document.getElementById('sideMenu');
-    menu.classList.toggle('open');
-}
+    function toggleMenu() {
+        const menu = document.getElementById("sideMenu");
+        menu.classList.toggle("active");
+    }
 </script>
